a4a758d5efeba8e7f500a191275f378eb403a312
David McGuigan <davide.mcguigan@gmail.com>
2024-10-20 20:54:20 -0300

    Some cleanup.

--- src/silentplanet-three-app/make-these-libs/three-world-stage/modules/StarrySky/StarrySky.js
+++ src/silentplanet-three-app/make-these-libs/three-world-stage/modules/StarrySky/StarrySky.js
@@ -1,5 +1,23 @@
 import * as THREE from 'three';
 
+/**
+ * @class StarrySky
+ * @description A class for creating a starry sky in a three.js scene.
+ * @param {THREE.Scene} scene - The scene to add the starry sky to.
+ * @param {THREE.Camera} camera - The camera to use for the starry sky.
+ * @param {THREE.CubeTexture} envMap - The environment map to use for the starry sky.
+ * 
+ * @example 
+ * const scene = new THREE.Scene();
+ * const camera = new THREE.Camera();
+ * const envMap = new THREE.CubeTextureLoader().load(texturePaths);
+ * const starrySky = new StarrySky(scene, camera, envMap);
+ * starrySky.initialize(texturePaths);
+ * 
+ * @see https://threejs.org/docs/#api/en/textures/CubeTexture
+ *  
+ * @todo: asyncronously load increasingly detailed starry sky textures
+ */
 export class StarrySky {
   constructor(scene, camera, envMap) {
     this.scene = scene;
